decaadd422877a419c1adc0ea883e57418756918
SaiKarun-28 <isaikarun536@gmail.com>
2026-03-09 15:12:26 +0530

    Nested loops.

---
+++ 09th_mar.py
@@ -0,0 +1,20 @@
+# Numbers
+
+for x in range(3):
+    for y in range(1,4):
+        print(x+y,end = ' ')
+    print()
+
+# alphabets
+
+for x in range(3):
+    for y in range(0,3):
+        print(chr(x+y+97),end = ' ')
+    print() 
+    
+# uppercase
+
+for x in range(3):
+    for y in range(0,3):
+        print(chr(x+y+65),end = ' ')
+    print()
