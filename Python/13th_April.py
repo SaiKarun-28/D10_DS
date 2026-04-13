0679204d7ea954737e7292b26b9839af4f96de4f
SaiKarun-28 <isaikarun536@gmail.com>
2026-04-13 13:45:38 +0530

    OOPS

--- Python/13th_April.py
+++ Python/13th_April.py
@@ -15,18 +15,47 @@ class Car():
         print('Car price :',self.price)
         print('Car speed :',self.speed)
         print('Car stopped at',self.break1)
+        print('_'*22)
+    
+    def start(self):
+        print(f'{self.name} started')
+        print('_'*22)
     
     def accelerate(self):
         if self.speed > 100:
             print('Limit reached.')
+            self.speed -= 30
+            print(f'Limit reached so car speed reduced to {self.speed}')
         else:
             self.speed += 10
+            print(self.speed)
+        print('_'*22)
     
     def stop(self):
-        if self.break1 == 0:
+        if self.break1 < 0:
             print("Can't decrease the speed.")
+            self.break1 += 10
+            print(f'Car started at speed {self.speed}')
         else:
-            break1 -= 10
-    
+            self.break1 -= 10
+            print(self.break1)
+        print('_'*22)
+        
+# First object
 c1 = Car('Cyber Truck','Red','Tesla',5000000,75,15)
 c1.info()
+c1.accelerate()
+c1.stop()
+c1.start()
+
+# Second Object
+c2 = Car('BMW X7','Blue','BMW',25000000,125,0)
+c2.start()
+c2.accelerate()
+c2.stop()
+
+# Third Object
+c3 = Car('R8','Red','AUDI',10000000,140,6)
+c3.start()
+c3.accelerate()
+c3.stop()
